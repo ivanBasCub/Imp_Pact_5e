dd47a4e10d5fdb04e3c44e3be887d5896ddcb373
Ivan Bascones <ibascub2017@hotmail.com>
2025-03-09 19:45:16 +0100

    Creacion de los componentes del SRD y sus enlaces

---
+++ src/componentes/SRD.jsx
@@ -0,0 +1,27 @@
+
+export default function SRD() {
+  return (
+    <div>
+      <h1>SRD</h1>
+      <p>Esta es la página principal del SRD</p>
+    </div>
+  )
+}
+
+function SpellList() {
+  return (
+    <div>
+      <h1>SpellList</h1>
+      <p>Esta es la lista de hechizos</p>
+    </div>
+  )
+}
+
+function Spell() {
+    return (
+        <div>
+        <h1>Spell</h1>
+        <p>Esta es la descripción de un hechizo</p>
+        </div>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './assets/css/index.css'
 import App from './componentes/App.jsx'
+import { SRD } from './componentes/SRD/*.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* Configuración de las rutas */}
     <BrowserRouter>
       <Routes>
+        {/* Página principal */}
         <Route path="/" element={<App />} />
+
+        {/* Rutas para el elementos del SRD */}
+        <Route path='/SRD' element={<SRD />} />
+        <Route path='/SRD/SpellList' element={<SpellList />} />
+        {/*
+        <Route path='/SRD/Spell/:id' element={<Spell />} />
+        */}
       </Routes>  
     </BrowserRouter>
   </StrictMode>,
