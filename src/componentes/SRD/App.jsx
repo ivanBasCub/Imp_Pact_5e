7ebbafebfb7b93f97f730208912b971f1f9f64cf
Ivan Bascones <ibascub2017@hotmail.com>
2025-04-01 18:38:15 +0200

    Implementación de SRD_Feats (Queda de mostrar los prerequisitos de las feats y modificaciones en las direcciones URL y creación de la Pantalla principal del SRD

---
+++ src/componentes/SRD/App.jsx
@@ -0,0 +1,10 @@
+import { useEffect, useState } from "react"
+import { useParams, Link } from "react-router-dom"
+
+export default function App(){
+    return (
+        <div>
+            <h1>Welcome to the SRD</h1>
+        </div>
+    )
+}
--- src/componentes/SRD/SRD.jsx
+++ src/componentes/SRD/SRD.jsx
@@ -2,34 +2,42 @@
 import { Routes, Route } from 'react-router-dom'
 
 // Compomentes
+import App from './App';
 import { SpellList, Spell } from "./Spells/Spell"
 import { Monster, MonsterList } from "./Monsters/Monster"
 import { MagicItemList, MagicItem } from './Magic_Items/magic_item'
 import { RaceList, Race } from './Races/Race'
-import Rules from './Rules/Rules';
+import Rules from './Rules/Rules'
+import {FeatsList, Feat } from './Feats/Feats'
 
 function SRD() {
   return (
     <Routes>
+      {/* Pagina Principal del SRD */}
+      <Route path='/SRD/' element={<App />} />
       {/* Reglas del DnD 5e */}
       <Route path='/SRD/Rules' element={<Rules />} />
 
       {/* Spells */}
-      <Route path='/SRD/SpellList/' element={<SpellList />} />
-      <Route path='/SRD/SpellList/:clase' element={<SpellList />} />
+      <Route path='/SRD/Spells/' element={<SpellList />} />
+      <Route path='/SRD/Spells/:clase' element={<SpellList />} />
       <Route path='/SRD/Spell/:id' element={<Spell />} />
 
       {/* Objetos Mágicos */}
-      <Route path='/SRD/MagicItemList/' element={<MagicItemList />} />
+      <Route path='/SRD/MagicItems/' element={<MagicItemList />} />
       <Route path='/SRD/MagicItem/:id' element={<MagicItem />} />
 
       {/* Monstruos */}
-      <Route path='/SRD/MonsterList' element={<MonsterList />} />
+      <Route path='/SRD/Monsters' element={<MonsterList />} />
       <Route path='/SRD/Monster/:id' element={<Monster />} />
 
       {/* Razas */}
       <Route path='/SRD/Race' element={<RaceList />} />
       <Route path='/SRD/Race/:id' element={<Race />} />
+
+      {/* Feats */}
+      <Route path='/SRD/Feats' element={<FeatsList />} />
+      <Route path='/SRD/Feat/:id' element={<Feat />} />
 
     </Routes>
   )
