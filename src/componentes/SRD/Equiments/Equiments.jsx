0f2a61a6b87893d77b273d38f5032afea40070d2
ivanBasCub <ibascub2017@hotmail.com>
2025-04-02 13:34:24 +0200

    Creacion de las tablas de SRD_Equipments y finalizado

--- src/componentes/SRD/Equiments/Equiments.jsx
+++ src/componentes/SRD/Equiments/Equiments.jsx
@@ -9,7 +9,7 @@ import { doc, setDoc, getDoc, collection, getDocs } from "firebase/firestore";
 const URL = "https://www.dnd5eapi.co";
 
 function EquimentList() {
-    const nameCollection = "SRD_Equiments";
+    const nameCollection = "SRD_Equipments";
     const equiemntsTypes = ["weapon", "armor", "adventuring-gear", "mounts-and-vehicles", "tools"];
 
     useEffect(() => {
@@ -20,8 +20,6 @@ function EquimentList() {
             const list = data.results;
             const listPromises = list.map(equiment => fetch(`${URL}${equiment.url}`).then(res => res.json()));
             const listEquiment = await Promise.all(listPromises);
-
-            console.log(listEquiment.filter(equiment => equiment.equipment_category.index === "tools"));
 
             listEquiment.forEach(async equiment => {
                 const equimentRef = doc(db, nameCollection, equiment.index);
@@ -47,7 +45,8 @@ function EquimentList() {
                             damage: damage,
                             properties: properties,
                             cost: equiment.cost,
-                            special: equiment.special
+                            special: equiment.special,
+                            weight: equiment.weight,
                         })
                     }
 
@@ -61,6 +60,7 @@ function EquimentList() {
                             armor_category: equiment.armor_category,
                             armor_class: equiment.armor_class,
                             stealth_disadvantage: equiment.stealth_disadvantage,
+                            weight: equiment.weight,
                             str_minimun: equiment.str_minimum,
                             properties: equiment.properties,
                             special: equiment.special
@@ -72,6 +72,8 @@ function EquimentList() {
                             index: equiment.index,
                             name: equiment.name,
                             desc: equiment.desc,
+                            cost: equiment.cost,
+                            weight: equiment.weight || 0,
                             equipment_category: equiment.equipment_category.index,
                             gear_category: equiment.gear_category.index,
                             properties: equiment.properties,
@@ -86,6 +88,8 @@ function EquimentList() {
                             index: equiment.index,
                             name: equiment.name,
                             desc: equiment.desc,
+                            cost: equiment.cost,
+                            weight: equiment.weight || 0,
                             equipment_category: equiment.equipment_category.index,
                             speed: speed,
                             properties: equiment.properties,
@@ -98,6 +102,8 @@ function EquimentList() {
                             index: equiment.index,
                             name: equiment.name,
                             desc: equiment.desc,
+                            cost: equiment.cost,
+                            weight: equiment.weight || 0,
                             equipment_category: equiment.equipment_category.index,
                             tool_category: equiment.tool_category,
                             properties: equiment.properties,
@@ -158,8 +164,8 @@ function EquimentList() {
 
 function Equiments() {
     const id = useParams();
-    const [ equipmentCategoryList, setEquipmentCategoryList ] = useState([]);
-    const nameCollection = "SRD_Equiments";
+    const [equipmentCategoryList, setEquipmentCategoryList] = useState([]);
+    const nameCollection = "SRD_Equipments";
 
     useEffect(() => {
 
@@ -167,7 +173,7 @@ function Equiments() {
             const ref = collection(db, nameCollection);
             const documents = await getDocs(ref);
             const filteredEquipments = [];
-        
+
             documents.forEach(document => {
                 const data = document.data();
                 if (data.equipment_category === id.id) {
@@ -179,12 +185,111 @@ function Equiments() {
         }
 
         fetchEquipments();
-    },[])
+    }, [])
 
 
-    return(
+    return (
         <div>
-            {console.log(equipmentCategoryList)}
+            <h1>
+                {id.id === "weapon" ? (<>Weapons</>) : ""}
+                {id.id === "armor" ? (<>Armors</>) : ""}
+                {id.id === "adventuring-gear" ? (<>Adventuring gear</>) : ""}
+                {id.id === "mounts-and-vehicles" ? (<>Mounts and Vehicules</>) : ""}
+                {id.id === "tools" ? (<>Tools</>) : ""}
+            </h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        {id.id === "weapon" ? (
+                            <>
+                                <th>Weapon Category</th>
+                                <th>Attack Type</th>
+                                <th>Damage</th>
+                                <th>Properties</th>
+                                <th>Weight</th>
+                            </>
+                        ) : ""}
+                        {id.id === "armor" ? (
+                            <>
+                                <th>Armor type</th>
+                                <th>AC</th>
+                                <th>Strength</th>
+                                <th>Stealth</th>
+                                <th>Weight</th>
+                            </>
+                        ) : ""}
+                        {id.id === "adventuring-gear" ? (
+                            <>
+                                <th>Description</th>
+                                <th>Weight</th>
+                            </>
+                        ): ""}
+                        {id.id === "mounts-and-vehicles" ? (
+                            <>
+                                <th>Speed</th>
+                                <th>Weight</th>
+                            </>
+                        ) : ""} 
+                        {id.id === "tools" ? (
+                            <>
+                                <th>Category</th>
+                                <th>Description</th>
+                                <th>Weight</th>
+                            </>
+                        ) : ""}                       
+
+                        <th>Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {console.log(equipmentCategoryList)}
+                    {equipmentCategoryList.map(equipment => (
+                        <tr>
+                            <td>{equipment.name}</td>
+                            {equipment.equipment_category === "weapon" ? (
+                                <>
+                                    <td>{equipment.weapon_category}</td>
+                                    <td>{equipment.weapon_range}</td>
+                                    <td>{equipment.weapon_range === "Ranged" ? (<>{equipment.damage.dice} {equipment.damage.type} ({equipment.range.normal} ft / {equipment.range.long}ft)</>) : (<>{equipment.damage.dice} {equipment.damage.type}</>)}</td>
+                                    <td>{equipment.properties.join(", ")}</td>
+                                    <td>{equipment.weight} lb</td>
+                                </>
+                            ) : ""}
+                            {equipment.equipment_category === "armor" ? (
+                                <>
+                                    <td>{equipment.armor_category}</td>
+                                    <td>{equipment.armor_class.base} {equipment.armor_class.dex_bonus === true ? (<>+ Dex modifier {equipment.armor_class.max_bonus ? (<>(max {equipment.armor_class.max_bonus})</>) : ""} </>) : ""}</td>
+                                    <td>{equipment.str_minimun != 0 ? (<>{equipment.str_minimun}</>): (<> - </>)}</td>
+                                    <td>{equipment.stealth_disadvantage === true ? (<>Disadvantage</>): (<> - </>)}</td>
+                                    <td>{equipment.weight} lb</td>
+
+                                </>
+                            ) : ""}
+                            {equipment.equipment_category === "adventuring-gear" ? (
+                                <>
+                                    <td>{equipment.desc.join(" ")}</td>
+                                    <td>{equipment.weight} lb</td>
+                                </>
+                            ) : ""}
+                            {equipment.equipment_category === "mounts-and-vehicles" ? (
+                                <>
+                                    <td>{equipment.speed != null ? (<> {equipment.speed.quantity} {equipment.speed.unit}</>) : (<> - </>) }</td>
+                                    <td>{equipment.weight} lb</td>
+                                </>
+                            ) :""}
+                            {equipment.equipment_category === "tools" ?(
+                                <>
+                                    <td>{equipment.tool_category}</td>
+                                    <td>{equipment.desc.join(" ")}</td>
+                                    <td>{equipment.weight} lb</td>
+                                </>
+                            ) : ""}
+                            <td>{equipment.cost.quantity}{equipment.cost.unit}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     )
 }
